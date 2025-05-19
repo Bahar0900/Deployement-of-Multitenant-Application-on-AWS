a08162fc16d387118b1e92ac74f13a8b4042f1c1
bahar0900 <sagormdsagorchowdhury@gmail.com>
2025-05-19 12:35:14 +0600

    Initial commit of Flask multitenant app with Citus and Docker

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
