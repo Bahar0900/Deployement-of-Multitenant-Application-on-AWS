9c3aec8c12b44c74a1a28b39a74e1f390dddc36d
Bahar0900 <sagormdsagorchowdhury@gmail.com>
2025-05-29 06:04:30 +0000

    worker-node problem resolve

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:mysecretpassword@localhost:5432/postgres'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'postgresql://postgres:mysecretpassword@localhost:5432/postgres?sslmode=disable'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
